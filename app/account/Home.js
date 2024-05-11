0c57ae5072331c09da5af8690099f1e80d7f7d01
seiiftarek <seiftarekvlogz@gmail.com>
2024-05-11 04:07:40 +0300

    rendering components

--- app/account/Home.js
+++ app/account/Home.js
@@ -1,12 +1,16 @@
-import AdminHeader from "../admin/adminheader";
-import AdminProducts from "../admin/adminproducts";
+import Categories from "../../Components/Categories";
+import Header from "../../Components/Header";
+import Products from "../../Components/Products";
 import { View, StyleSheet, ScrollView } from "react-native";
+import AboveHeader from "../../Components/AboveHeader";
 export default function Home() {
   return (
     <View style={styles.container}>
       <ScrollView>
-        <AdminHeader />
-        <AdminProducts />
+        <AboveHeader />
+        <Header />
+        {/* <Categories /> */}
+        <Products />
       </ScrollView>
     </View>
   );
@@ -15,6 +19,7 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: "column",
     alignItems: "center",
     backgroundColor: "white",
   },
